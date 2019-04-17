c1818160af21fd82921410c11d2dd65349aa2a9d
Jake Napper <napperjlg@gmail.com>
2019-04-17 15:35:20 +0100

    add post-redirect loop for player names

--- app.rb
+++ app.rb
@@ -1,14 +1,29 @@
+
+
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  set :session_secret, 'super secret'
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect "/play"
+
+  end
+
+  get '/play' do
+
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
+
   end
 
 
